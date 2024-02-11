9e79bdbf979b2da9473e4246e4f3ffe5f70f6504
METEHANBYRKDR <102472877+METEHANBYRKDR@users.noreply.github.com>
2024-02-11 19:51:36 +0300

    Add project files.

---
+++ Business/Concrete/PersonMenager.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PersonMenager
+{
+    //encapsulation
+    public void ApplyForMask(Person person)
+    {
+
+    }
+
+    public List<Person> GetList()
+    {
+        return null;
+    }
+}
